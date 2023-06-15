ccdfa865fdf9b783e0ee071d4b28698f63c44c32
unknown <sultu9242arshiya@gmail.com>
2023-06-15 23:14:48 +0530

    Project completed

---
+++ src/com/datatype/DataTypes.java
@@ -0,0 +1,38 @@
+package com. datatype;
+
+public class DataTypes {
+	public static void main(String[] args) {
+		
+		System.out.println("Bio Data");
+		System.out.println("*********");
+
+		byte age = 23;
+		System.out.println("My age is:" + age);
+
+		short empId = 1254;
+		System.out.println("My empId is:" + empId);
+
+		int pincode = 600073;
+		System.out.println("My pincode is:" + pincode);
+
+		long phNo = 7894562310l;
+		System.out.println("My phone no is:" + phNo);
+
+		float salary = 10000f;
+		System.out.println("My salary is:" + salary);
+
+		double pf = 200.53;
+		System.out.println("My pf is:" + pf);
+
+		char gender = 'F';
+		System.out.println("My gender is:" + gender);
+
+		String address = "No:15 Iaf Road, " + "Selaiyur, " +  "Chennai";
+		System.out.println("My address is:" + address);
+
+		boolean check= true ;
+		System.out.println(check);
+
+	}
+
+}
